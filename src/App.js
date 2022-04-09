f5694e35172b73a9b29bad756d10d157b483cf8b
FullSol <ceraines@student.fullsail.edu>
2022-04-09 09:17:37 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meal/Meals";
+
+const App = () => {
+	return (
+		<>
+			<Header />
+			<main>
+				<Meals />
+			</main>
+		</>
+	);
+};
+export default App;
